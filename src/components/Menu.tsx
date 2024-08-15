3317f7c5d5be9866f3b4e3ea1d5b433f4b558000
R1c4ar6 <bleaksoulless@gmail.com>
2024-08-15 15:13:13 +0200

    Adding description to the menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,18 +1,32 @@
-import reactLogo from '../assets/react.svg';
+import reactLogo from "../assets/react.svg";
 
 function Menu() {
   return (
     <>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container">
-          <a className="navbar-brand" href="#">
-            <img
-              src={reactLogo}
-              alt="React Logo"
-              width="60"
-              height="60"
-            />
+        <div className="container-fluid p-2 d-flex justify-content-between">
+          <a className="navbar-brand" href="#" id="menu-logo">
+            <img src={reactLogo} alt="React Logo" width="60" height="60" /> Portfolio Richard Jiménez
           </a>
+          <div className="collapse navbar-collapse" id="menu-content">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     </>
